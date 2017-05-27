e31f94941272d876056e87fe4dc246b994ca92a4
Theophile-Molcard <theophile.molcard@etu.utc.fr>
2017-05-27 12:37:06 +0200

    NotesManager
    Iterator + singleton

--- Notes.h
+++ Notes.h
@@ -1,9 +1,78 @@
 #ifndef NOTES_H
 #define NOTES_H
 
-class Notes{
+#include <QDateTime>
+
+using namespace std;
+
+class Note{
 public:
+
 private:
+    string identificateur;
+    string titre;
+    QDateTime date_creation;
+    QDateTime date_modif;
+};
+
+class NotesManager{
+public:
+    static NotesManager& donneInstance(){
+        if (!instance)
+            instance = new NotesManager();
+        return *instance;
+    }
+
+    static void supprimeInstance(){
+        if (instance){
+            delete instance;
+            instance = nullptr;
+        }
+    }
+
+    class Iterator {
+    public:
+        Note* operator *(){
+            return *current;
+        }
+        Note* operator ++(){
+            if(last == 0)
+                throw "il faut faire une calsse d'exceptions";
+            --last;
+            return *(++current);
+        }
+        Note* operator ++(int){
+            if(last == 0)
+                throw "il faut faire une calsse d'exceptions";
+            Note* save = *current;
+            ++current;
+            --last;
+            return save;
+        }
+
+    private:
+        Note** current;
+        unsigned int last;
+        friend class NotesManager;
+        Iterator(Note** _tab, unsigned int _nb_max)
+            : current(_tab), last(_nb_max) {}
+    };
+
+    Iterator getIterator() {
+        return Iterator(tab, nb_max);
+    }
+
+
+private:
+    Note** tab;
+    unsigned int nb;
+    unsigned int nb_max;
+
+    static NotesManager* instance;
+    NotesManager();
+    NotesManager(const NotesManager&);
+    virtual ~NotesManager();
+    void operator =(const NotesManager&);
 
 };
 

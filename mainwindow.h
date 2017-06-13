78b21578ecb8f544cc88395d40eab53ebc357fc6
TheoPortmann <portmann.theo@gmail.com>
2017-06-13 10:26:01 +0200

    debut commentaires mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -19,37 +19,95 @@
 #include <QWidget>
 #include <QSettings>
 
+/*!
+* \file mainwindow.h
+* \brief Définition des classes utiles à la fenetre principale
+* \author Théophile Maxime Théo
+* \version 0.99
+*/
 
-
+/*! \namespace Ui
+ * \brief contient la class MainWindow
+ */
 namespace Ui {
 class MainWindow;
 }
 
+/*! \class MainWindow
+ * \brief Classe fille de QMainWindow, permet de gérer la fenetre principale
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 
 public:
+    /*!
+     * \brief Constructeur de MainWindow
+     * \param parent QWidget parent
+     */
     explicit MainWindow(QWidget *parent = 0);
+    /*!
+     * \brief destructeur de MainWindow
+     */
     ~MainWindow();
 
     // Create()
+    /*!
+     * \brief Créer une fenetre pour éditer un article
+     */
     void editeur_article();
+    /*!
+     * \brief Créer une fenetre pour éditer un multimedia
+     */
     void editeur_multimedia();
+    /*!
+     * \brief Créer une fenetre pour éditer une tache
+     */
     void editeur_tache();
 
     // Ouvrir()
+    /*!
+     * \brief ouvre l'éditeur de tache à partir des données d'une note
+     * \param note référence sur la note qui contient les information de la tache
+     */
     void ouvre_tache(Note& note);
+    /*!
+     * \brief ouvre l'éditeur de multimedia à partir des données d'une note
+     * \param note référence sur la note qui contient les information du multimedia
+     */
     void ouvre_multi(Note& note);
+    /*!
+     * \brief ouvre l'éditeur d'article à partir des données d'une note
+     * \param note référence sur la note qui contient les information de l'article
+     */
     void ouvre_article(Note& note);
 
+    /*!
+     * \brief ouvre l'aperçu d'une relation
+     * \param rela relation à ouvrir
+     */
     void visualiser_rela_specifique(QString rela);
 
+    /*!
+     * \brief booléen pour afficher l'arborescence des relations
+     * \return true si on affiche, false sinon
+     */
     bool bool_arbo() {return pref_arbo;}
 
     //Undo-Redo
+    /*!
+     * \brief deleteRelation
+     * \param rel
+     */
     void deleteRelation(Relation* rel);
+    /*!
+     * \brief deleteCouple
+     * \param rel
+     * \param _x
+     * \param _y
+     * \param _label
+     */
     void deleteCouple(QString rel, QString _x, QString _y, QString _label = "default");
 
 private:
